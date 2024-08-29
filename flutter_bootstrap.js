c82e6afe4ce3d90751700b25ecd9179041750bc4
Sen sathyamony <sensathyamony@gmail.com>
2024-08-29 14:01:40 +0700

    Change testing project

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1818734244"
+    serviceWorkerVersion: "3241923925"
   }
 });
